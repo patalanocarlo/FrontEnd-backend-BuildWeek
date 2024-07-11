89f8cfdc5a6bb851976454f87f9ade3a6d834be8
Carmen Romano <my-9@hotmail.it>
2024-07-11 23:26:47 +0200

    struttura dashboard

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,5 +1,70 @@
-let AdminDashboard = () => {
-  return <div>DASHBOARD ADMIN</div>;
+import * as React from "react";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Deposits from "./Deposits";
+import Orders from "./Orders";
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+const AdminDashboard = () => {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          {/* Chart */}
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper
+              sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                height: 240,
+              }}
+            ></Paper>
+          </Grid>
+          {/* Recent Deposits */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper
+              sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                height: 240,
+              }}
+            >
+              <Deposits />
+            </Paper>
+          </Grid>
+          {/* Recent Orders */}
+          <Grid item xs={12}>
+            <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+              <Orders />
+            </Paper>
+          </Grid>
+        </Grid>
+        {/* Footer */}
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          align="center"
+          sx={{ pt: 4 }}
+        >
+          {"Copyright "}
+          <Typography component="span" color="inherit">
+            Your Website
+          </Typography>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default AdminDashboard;
