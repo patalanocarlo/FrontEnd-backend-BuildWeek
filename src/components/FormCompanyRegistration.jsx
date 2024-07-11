22b7bf8757917c6feb3734788c27cbd69eca77bd
Carmen Romano <my-9@hotmail.it>
2024-07-11 23:00:22 +0200

    registrazione cliente

--- src/components/FormCompanyRegistration.jsx
+++ src/components/FormCompanyRegistration.jsx
@@ -1,3 +1,4 @@
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { Col, Container, Row, Form, FormGroup } from "react-bootstrap";
 
@@ -6,6 +7,13 @@ const FormCompanyRegistration = () => {
   const [comuni, setComuni] = useState([]);
   const [selectedProvince, setSelectedProvince] = useState("");
   const [selectedComune, setSelectedComune] = useState("");
+  const [ragioneSociale, setRagioneSociale] = useState("");
+  const [partitaIva, setPartitaIva] = useState("");
+  const [pec, setPec] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [nome, setNome] = useState("");
+  const [cognome, setCognome] = useState("");
+  const [telefonoPersonale, setTelefonoPersonale] = useState("");
   const [token, setToken] = useState(localStorage.getItem("authToken") || "");
 
   useEffect(() => {
@@ -48,52 +56,154 @@ const FormCompanyRegistration = () => {
     }
   };
 
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    const formData = {
+      ragioneSociale,
+      partitaIva,
+      pec,
+      telefono,
+      nome,
+      cognome,
+      telefonoPersonale,
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:3001/registration/cliente",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to register cliente");
+      }
+
+      const data = await response.json();
+      console.log("Registration successful:", data);
+      // Eseguire azioni supplementari dopo la registrazione
+    } catch (error) {
+      console.error("Errore durante la registrazione del cliente:", error);
+    }
+  };
+
   return (
     <div>
-      <Container className="resultsContainer">
-        <Row>
-          <Col sm="12" md="6" className="bg-white rounded border">
-            <br />
-            <h3>General Information:</h3>
-            <Form>
+      <Container>
+        <h1 className="text-center my-4"> Registrazione Cliente </h1>
+        <Form onSubmit={handleSubmit} className="p-4 rounded border">
+          <Row>
+            <Col sm="12" md="6" className="bg-white">
+              <br />
+              <h3>Informazioni Aziendali</h3>
               <FormGroup>
-                <Form.Label>Province</Form.Label>
                 <Form.Control
-                  as="select"
-                  name="province"
-                  id="province"
-                  value={selectedProvince}
-                  onChange={handleProvinceChange}
-                >
-                  <option value="">Select Province</option>
-                  {provinces.map(province => (
-                    <option key={province.id} value={province.id}>
-                      {province.name}
-                    </option>
-                  ))}
-                </Form.Control>
+                  type="text"
+                  placeholder="Ragione Sociale"
+                  value={ragioneSociale}
+                  onChange={e => setRagioneSociale(e.target.value)}
+                  className="my-4"
+                />
               </FormGroup>
               <FormGroup>
-                <Form.Label htmlFor="comune">Comune</Form.Label>
                 <Form.Control
-                  as="select"
-                  name="comune"
-                  id="comune"
-                  value={selectedComune}
-                  onChange={e => setSelectedComune(e.target.value)}
-                  disabled={!selectedProvince}
-                >
-                  <option value="">Select Comune</option>
-                  {comuni.map(comune => (
-                    <option key={comune.id} value={comune.id}>
-                      {comune.name}
-                    </option>
-                  ))}
-                </Form.Control>
+                  type="number"
+                  placeholder="Partita IVA"
+                  value={partitaIva}
+                  onChange={e => setPartitaIva(e.target.value)}
+                />
               </FormGroup>
-            </Form>
-          </Col>
-        </Row>
+              <FormGroup>
+                <Form.Control
+                  type="email"
+                  placeholder="PEC"
+                  value={pec}
+                  onChange={e => setPec(e.target.value)}
+                  className="my-4"
+                />
+              </FormGroup>
+              <FormGroup>
+                <FormControl fullWidth>
+                  <InputLabel id="province-label">
+                    Seleziona Provincia
+                  </InputLabel>
+                  <Select
+                    labelId="province-label"
+                    id="province"
+                    value={selectedProvince}
+                    onChange={handleProvinceChange}
+                  >
+                    {provinces.map(province => (
+                      <MenuItem key={province.id} value={province.id}>
+                        {province.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </FormGroup>
+              <FormGroup>
+                <FormControl fullWidth className="my-4">
+                  <InputLabel id="comune-label">Seleziona Comune</InputLabel>
+                  <Select
+                    labelId="comune-label"
+                    id="comune"
+                    value={selectedComune}
+                    onChange={e => setSelectedComune(e.target.value)}
+                    disabled={!selectedProvince}
+                  >
+                    {comuni.map(comune => (
+                      <MenuItem key={comune.id} value={comune.id}>
+                        {comune.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </FormGroup>
+            </Col>
+            <Col sm="12" md="6" className="bg-white border-left">
+              <br />
+              <h3>Contatto Azienda</h3>
+              <FormGroup className="my-4">
+                <Form.Control
+                  type="text"
+                  placeholder="Nome"
+                  value={nome}
+                  onChange={e => setNome(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Form.Control
+                  type="text"
+                  placeholder="Cognome"
+                  value={cognome}
+                  onChange={e => setCognome(e.target.value)}
+                  className="my-4"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Form.Control
+                  type="text"
+                  placeholder="Telefono Personale"
+                  value={telefonoPersonale}
+                  onChange={e => setTelefonoPersonale(e.target.value)}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <button
+            type="submit"
+            className="btn btn-outline-dark w-100 rounded-5 fs-5 mb-5"
+          >
+            Invia
+          </button>
+        </Form>
       </Container>
     </div>
   );
